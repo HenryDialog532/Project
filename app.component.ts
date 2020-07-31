f0f9d72e0708aa9d502bbea7a2f5626bf57af89a
Naninani553 <68081175+Naninani553@users.noreply.github.com>
2020-07-31 08:59:28 -0500

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+                // 'node_modules/bootstrap/dist/css/bootstrap.min.css',
+                // 'src/styles.css'
+  ]
+  
 })
 export class AppComponent {
   title = 'ACIwireframe';
   items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
-
+  showPDF(id){
+    debugger;
+  }
 }
